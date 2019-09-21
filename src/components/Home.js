a4bb1c9b1cb860f6816a18e1c8c4295a976d063c
Chris Norwood <cnorwood92@gmail.com>
2019-09-21 16:51:51 -0700

    Finish up Home route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { getTeamNames } from '../api'
+import TeamLogo from './TeamLogo'
 
 export default function Home () {
+  const [teamNames, setTeamNames] = React.useState([])
+
+  React.useEffect(() => {
+    getTeamNames().then(setTeamNames)
+  }, [])
+
   return (
     <div className='container'>
-      Home
+      <h1 className='large-header'>
+        Hash History Basketball League
+      </h1>
+      <h3 className='header text-center'>
+        Select a team
+      </h3>
+      <div className='home-grid'>
+        {teamNames.map(id => (
+          <Link key={id} to={`/${id}`}>
+            <TeamLogo id={id} width='125px' />
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
