92215cc07d2bbe7a97c93a231a4c35fff142c204
Chris Norwood <cnorwood92@gmail.com>
2019-09-22 08:13:11 -0700

    Complete basic Players and Sidebar component

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import { parse } from 'query-string'
+import slug from 'slug' 
+import { getPlayers } from '../api'
+import Sidebar from './Sidebar'
 
-export default function Players () {
+export default function Players (props) { 
+  const [players, setPlayers] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+  
+  const { match, location } = props
+
+  const fetchPlayers = (teamId) => {
+    getPlayers(teamId)
+      .then(players => {
+        setPlayers(players)
+        setLoading(false)
+      })
+  }
+
+  React.useEffect(() => {
+    location.search
+      ? fetchPlayers(parse(location.search).teamId)
+      : fetchPlayers()
+  }, [location])
+
   return (
     <div className='container two-column'>
-      Players
+      <Sidebar
+        loading={loading}
+        title='Players'
+        list={players.map(player => player.name)}
+        {...props}
+      />
+
+      {loading === false && location.pathname === '/players'
+        ? <div className='sidebar-instruction'>Select a Player</div>
+        : null}
     </div>
   )
 }
