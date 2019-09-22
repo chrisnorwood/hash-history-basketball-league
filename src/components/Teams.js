9e00d46c81135d18d6dbd6422bbeef958f6339b3
Chris Norwood <cnorwood92@gmail.com>
2019-09-22 08:33:25 -0700

    Render basic Teams view

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { getTeamNames } from '../api'
+import Sidebar from './Sidebar'
 
-export default function Teams () {
+export default function Teams (props) {
+  const [teamNames, setTeamNames] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  const { location , match } = props
+
+  React.useEffect(() => {
+    getTeamNames()
+      .then(teams => {
+        setTeamNames(teams)
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <div className='container two-column'>
-      Teams
+      <Sidebar
+        loading={loading}
+        title='Teams'
+        list={teamNames}
+        {...props}
+      />
+      
+      {loading === false && location.pathname === '/teams'
+        ? <div className='sidebar-instruction'>Select a Team</div>
+        : null}
     </div>
   )
 }
